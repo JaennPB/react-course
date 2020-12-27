34c508c3db3692d1f7b8fb475180789338a74c67
jaennpb <jaenn.bucio@gmail.com>
2020-12-27 12:46:39 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person.js";
+
+// component must always return JSX or somethig to render
+// class based component
+class App extends Component {
+  // property object (only availible on React components i.e: extends Component)
+  state = {
+    persons: [
+      { name: "Jaenn", age: 25 },
+      { name: "Phoebe", age: 3 },
+      { name: "Pau", age: 24 },
+    ],
+  };
+
+  render() {
+    return (
+      // tipicaly everything that should be rendered PER component should be inside it
+      <div className="App">
+        <h1>Hi, I am a React app</h1>
+        <p>this is really working!</p>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          {/* 'children' can be more html or plain text */} (is a cat)
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    // *** this is what the JSX above will be compiled to
+    // return React.createElement(
+    //   "div",
+    //   null,
+    //   React.createElement("h1", { className: "App" }, "Does this work now?")
+    // );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// at the most basic level, a react component is a js function that returns JSX (html) code which we want to render
+// JSX is just syntactic sugar for JavaScript, allowing you to write HTMLish code instead of nested React.createElement(...) calls.
+
+// functional component
+// props is a React object that gives us access to attributes passed in App
+const Person = (props) => {
+  return (
+    <div>
+      <p>
+        I am {props.name} and I am {props.age} years old
+      </p>
+      <span>{props.children}</span>
+    </div>
+  );
+};
+
+export default Person;
+
+// A typical React app therefore could be depicted as a component tree - having one root component ("App") and then an potentially infinite amount of nested child components.
