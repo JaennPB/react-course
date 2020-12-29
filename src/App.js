5ccecf148f563bf5dbc1257c142be8e6378063c1
jaennpb <jaenn.bucio@gmail.com>
2020-12-28 18:46:19 -0600

    commit 6

--- src/App.js
+++ src/App.js
@@ -41,12 +41,24 @@ class App extends Component {
   };
 
   render() {
+    // adding in-line styling to specific dom element
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+
     return (
       // tipicaly everything that should be rendered PER component should be inside it
       <div className="App">
         <h1>Hi, I am a React app</h1>
         <p>this is really working!</p>
-        <button onClick={this.switchNameHandler.bind(this, "JAY!!!")}>
+        <button
+          onClick={this.switchNameHandler.bind(this, "JAY!!!")}
+          style={style}
+        >
           Change names
         </button>
         <Person
