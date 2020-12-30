67834dfd4bb28ac7e5642ee4be3960e95885ee10
jaennpb <jaenn.bucio@gmail.com>
2020-12-29 22:30:28 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person.js";
 
-// component must always return JSX or somethig to render
-// class based component
 class App extends Component {
-  // property object 'state' can only be accessed in class based components
   state = {
     // !!! changes to state trigger UI update !!!
     persons: [
@@ -13,11 +10,10 @@ class App extends Component {
       { name: "Phoebe", age: 4 },
       { name: "Pau", age: 24 },
     ],
-    otherProp: "will be left untouched",
+    showPersons: false,
   };
 
   switchNameHandler = (name) => {
-    // console.log("was clicked");
     // DON'T DO THIS: this.state.persons[0].name = 'Jay'
     this.setState({
       // will merge only changed values with state
@@ -27,6 +23,11 @@ class App extends Component {
         { name: "Pau", age: 25 },
       ],
     });
+  };
+
+  togglePersonsHandler = () => {
+    const show = this.state.showPersons;
+    this.setState({ showPersons: !show });
   };
 
   changeNameHandler = (e) => {
@@ -55,39 +56,33 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a React app</h1>
         <p>this is really working!</p>
-        <button
-          onClick={this.switchNameHandler.bind(this, "JAY!!!")}
-          style={style}
-        >
-          Change names
+        <button onClick={this.togglePersonsHandler} style={style}>
+          Show list
         </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          changed={this.changeNameHandler}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          {/* 'children' can be more html or plain text */} (is a cat)
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          // passing method references between components as props and using bind so that we can call certain method with specified argument
-          click={this.switchNameHandler.bind(this, "JAY!!!")}
-        />
+        {this.state.showPersons ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              changed={this.changeNameHandler}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+            >
+              {/* 'children' can be more html or plain text */} (is a cat)
+            </Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+              // passing method references between components as props and using bind so that we can call certain method with specified argument
+              click={this.switchNameHandler.bind(this, "JAY!!!")}
+            />
+          </div>
+        ) : null}
       </div>
     );
-    // *** this is what the JSX above will be compiled to
-    // return React.createElement(
-    //   "div",
-    //   null,
-    //   React.createElement("h1", { className: "App" }, "Does this work now?")
-    // );
   }
 }
 
-// react listens for changes in STATE and PROPS, they will TRIGGER a RENDER or 'react' to changes made, and will update UI accordingly
 export default App;
