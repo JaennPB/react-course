00e88014441652e22d73b0c70690f6ac31100d18
jaennpb <jaenn.bucio@gmail.com>
2020-12-29 23:13:26 -0600

    conditionally rendering

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,25 +1,12 @@
 import React from "react";
-// css stylesheets must be imported so that webpack can add them to HTML when compiling/building
-// css will NOT be merged with js, it will only be added to final html compiled file
-// automatic prefixing will happen! by webpack
 import "./Person.css";
 
-// at the most basic level, a react component is a js function that returns JSX (html) code which we want to render
-// JSX is just syntactic sugar for JavaScript, allowing you to write HTMLish code instead of nested React.createElement(...) calls.
-
-// functional component
-// props is a React object that gives us access to attributes passed in App
-
-// this is also called a stateless component, its good practice to have more stateless components than stateful components
-// so that the logic of the app can live in a few stateful components rather than been broken into small pieces all over the place
 const Person = (props) => {
   return (
     <div className="Person">
-      {/* passing method references between components as props, so that a component that doesnt have access to the state or a certain method, can change the state from outside or access certain methods */}
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} years old
       </p>
-      {/* !!! props also trigger a UI render !!! */}
       <p>{props.children}</p>
       <input
         type="text"
@@ -31,5 +18,3 @@ const Person = (props) => {
 };
 
 export default Person;
-
-// A typical React app therefore could be depicted as a component tree - having one root component ("App") and then an potentially infinite amount of nested child components.
