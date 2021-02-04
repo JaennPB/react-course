60339040f2f98dbbe9a5b945a22710aabb04ab0e
jaennpb <jaenn.bucio@gmail.com>
2021-02-04 09:09:50 -0600

    commit

--- src/hoc/withAddedClass.js
+++ src/hoc/withAddedClass.js
@@ -3,8 +3,9 @@ import React from 'react';
 const withAddedClass = (WrappedComponent, className) => {
   return (props) => (
     <div className={className}>
-      <WrappedComponent />
+      <WrappedComponent {...props} />
     </div>
+    // spreading props into wrapped component so that props can be rendered
   );
 };
 
